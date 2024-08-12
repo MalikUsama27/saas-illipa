5ad31fa27e0d46a3f45d7c54089ef64dd39bcc62
Usama-Ashraf27 <usama@karigar.pk>
2024-08-12 06:44:57 -0700

    slash url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Login from "./components/auth/Login";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import Modules from "./components/Home/Modules";
+import Customer from "./components/Home/Customer";
+import RevenuePlan from "./components/Home/RevenuePlan";
+// import AddRevenuePlan from './components/Home/AddRevenuePlan';
+import Users from "./components/Home/Users";
+// import AddUser from './components/Home/AddUser';
+// import AddCustomer from './components/Home/AddCustomer';
+import Receipt from "./components/Home/Receipt";
+import RevenueProjection from "./components/Home/RevenueProjection";
 
-const App = () => {  console.log('App')
+function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      if (location.pathname === "/") {
+        navigate("/dashboard/modules");
+      }
+    } else {
+      if (location.pathname !== "/") {
+        navigate("/login");
+      }
+    }
+  }, [navigate, location.pathname]);
 
   return (
-  <div>
-    <h1>  App </h1>
-
-    </div>
-  )
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/" element={<h1>Hello</h1>} />
+      <Route exact path="/forgot-password" element={<ForgotPassword />} />
+      <Route exact path="/dashboard" element={<Layout />}>
+        <Route exact path="modules" element={<Modules />} />
+        <Route exact path="customers" element={<Customer />} />
+        <Route exact path="revenue-plan" element={<RevenuePlan />} />
+        {/* <Route exact  path="add-revenue" element={<AddRevenuePlan />} /> */}
+        <Route exact path="users" element={<Users />} />
+        {/* <Route exact  path="add-user" element={<AddUser />} /> */}
+        {/* <Route exact  path="add-customer" element={<AddCustomer/>}/> */}
+        <Route exact path="receipt" element={<Receipt />} />
+        <Route
+          exact
+          path="revenue-projection"
+          element={<RevenueProjection />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
