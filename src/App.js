0ecb8ccaf4e669cbb510821049f0d804261bd3d0
Usama-Ashraf27 <usama@karigar.pk>
2024-08-12 07:33:06 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -22,10 +22,11 @@ function App() {
     console.log("path",location.pathname);
     if (token) {
       if (location.pathname === "/"  )  {
-        navigate("/dashboard/modules");
-      }else if(location.pathname ==='/dashboard/modules'){
-        navigate("/dashboard/modules");
+        navigate("/modules");
       }
+      // else if(location.pathname ==='/modules'){
+      //   navigate("/modules");
+      // }
     } else {
       if (location.pathname !== "/") {
         navigate("/");
@@ -38,17 +39,17 @@ function App() {
       <Route exact path="/" element={<Login />} />
       <Route exact path="/forgot-password" element={<ForgotPassword />} />
       {/* <Route exact path="/dashboard" element={<Layout />}> */}
-        <Route exact path="modules" element={<Modules />} />
-        <Route exact path="customers" element={<Customer />} />
-        <Route exact path="revenue-plan" element={<RevenuePlan />} />
+        <Route exact path="/modules" element={<Modules />} />
+        <Route exact path="/customers" element={<Customer />} />
+        <Route exact path="/revenue-plan" element={<RevenuePlan />} />
         {/* <Route exact  path="add-revenue" element={<AddRevenuePlan />} /> */}
-        <Route exact path="users" element={<Users />} />
+        <Route exact path="/users" element={<Users />} />
         {/* <Route exact  path="add-user" element={<AddUser />} /> */}
         {/* <Route exact  path="add-customer" element={<AddCustomer/>}/> */}
-        <Route exact path="receipt" element={<Receipt />} />
+        <Route exact path="/receipt" element={<Receipt />} />
         <Route
           exact
-          path="revenue-projection"
+          path="/revenue-projection"
           element={<RevenueProjection />}
         />
       {/* </Route> */}
